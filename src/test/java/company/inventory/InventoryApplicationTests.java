c34c45a70f5d1cbd6a309fe6acc6efb08d07cf64
Sergio Flores <sergioflores.vt@gmail.com>
2023-07-12 21:51:37 -0300

    Subiendo proyecto

---
+++ src/test/java/company/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package company.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
